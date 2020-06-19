b879699a98941ffb6b291a93b2206739ace32524
Akriti Goswami <akritigoswami12@gmail.com>
2020-06-19 13:58:42 +0530

    Updates

---
+++ precache-manifest.55585803b1f9b1b7c45e4764a333f628.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1dbe1c4e65cd4aaae29cdf41778e51b",
+    "url": "/React-TODO/index.html"
+  },
+  {
+    "revision": "4625787f34d77bef30a8",
+    "url": "/React-TODO/static/css/main.2f85b384.chunk.css"
+  },
+  {
+    "revision": "faef0401ff180e578be4",
+    "url": "/React-TODO/static/js/2.ee62a020.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-TODO/static/js/2.ee62a020.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4625787f34d77bef30a8",
+    "url": "/React-TODO/static/js/main.dfea1f88.chunk.js"
+  },
+  {
+    "revision": "37c532c8017f7c9aa12b",
+    "url": "/React-TODO/static/js/runtime-main.ab8fe602.js"
+  }
+]);
